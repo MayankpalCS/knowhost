50a5e9c0417f856d179de0c8527b2dd5737be0a3
Mayankpalcomputers <110147408+Mayankpalcomputers@users.noreply.github.com>
2022-08-07 09:27:25 +0530

    Update arpfind.py

--- arpfind.py
+++ arpfind.py
@@ -3,7 +3,7 @@ import ipaddress
 from scapy.all import srp
 from scapy.layers.l2 import ARP
 from scapy.layers.l2 import ARP ,Ether
-from colorama import init, Fore
+
 def banner():
     print("""%s
             |  / ||   | |------| |     | |    | |----| --------- ---------
@@ -15,9 +15,8 @@ def banner():
                            """)
 banner()
 target=sys.argv[1]
-init()
-red=Fore.RED
-green=Fore.GREEN
+
+
 #a list to sstore hosts
 print("demo:python3 arpfinder.py 192.168.159.0/24")
 online_host=[]
@@ -32,7 +31,7 @@ result=srp(packet, timeout=4)
 uphost = result[0]
 for sent, received in uphost:
     online_host.append({'ip': received.psrc, 'mac': received.hwsrc})
-print(f"{green}uphosts:")
-print(f"{green}IP\t\t\t\t\tMAC")
+print("uphosts:")
+print("IP\t\t\t\t\tMAC")
 for host in online_host:
-    print(f"{green}[+]{}\t\t{}".format(host['ip'], host['mac']))
+    print("[+]{}\t\t{}".format(host['ip'], host['mac']))
