e2a59c0510ec0a8a2b3b616e8818cd25d8e5d0db
Mayankpalcomputers <110147408+Mayankpalcomputers@users.noreply.github.com>
2022-08-07 09:01:28 +0530

    Create arpfind.py
    usage: python3 main.py 192.168.159.0/24

---
+++ arpfind.py
@@ -0,0 +1,24 @@
+import sys
+import ipaddress
+from scapy.all import srp
+from scapy.layers.l2 import ARP
+from scapy.layers.l2 import ARP ,Ether
+target=sys.argv[1]
+#a list to sstore hosts
+print("demo:python3 arpfinder.py 192.168.159.0/24
+online_host=[]
+#ff:ff:ff:ff:ff:ff is broadcast adress
+ether=Ether(dst="ff:ff:ff:ff:ff:ff")
+arp=ARP(pdst=target)
+enscapsulating ip and arp packets
+packet=ether/arp
+
+#setting timeout 
+result=srp(packet, timeout=4)
+uphost = result[0]
+for sent, received in uphost:
+    online_host.append({'ip': received.psrc, 'mac': received.hwsrc})
+print("uphosts:")
+print("IP\t\t\t\t\tMAC")
+for host in online_host:
+    print("[+]{}\t\t{}".format(host['ip'], host['mac']))
